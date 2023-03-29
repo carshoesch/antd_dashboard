d1fef9ac56a11344ff63270b680a19fd89e2e3f2
carshoesch <hcarsten1@googlemail.com>
2023-03-29 13:53:08 +0200

    Removed errors that occourred from merging

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,15 +4,16 @@ import dbLogo from '../../assets/img/VIER-Logo.png';
 import { BellOutlined, MailOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { BsMoon, BsMoonFill } from 'react-icons/bs'
 import SetMode from '../Mode/SetMode';
 
 const Header = () => {
     /**
-     * Notifsications = comments
+     * Notifications = comments
      * Messages = posts
      */
     const [openMessages, setOpenMessages] = useState(false);
-    const [openNotifs, setOpenNotifs] = useState(false);
+    const [openNotif, setOpenNotif] = useState(false);
     const [messages, setMessages] = useState([]);
     const [notif, setNotif] = useState([]);
 
@@ -22,10 +23,6 @@ const Header = () => {
     }, [openMessages, openNotif]);
 
     const fetchMessages = async () => {
-        const result = await axios.get(process.env.REACT_APP_NOTIF_URL);
-        setNotifs(result.data.posts);
-    };
-    const fetchNotifs = async () => {
         const result = await axios.get(process.env.REACT_APP_MESSAGES_URL);
         setMessages(result.data.posts);
     };
@@ -39,99 +36,72 @@ const Header = () => {
     const handleCloseMessages = () => {
         setOpenMessages(false);
     };
-    const handleOpenNotifs = () => {
-        setOpenNotifs(true);
+    const handleOpenNotif = () => {
+        setOpenNotif(true);
     };
-    const handleCloseNotifs = () => {
-        setOpenNotifs(false);
+    const handleCloseNotif = () => {
+        setOpenNotif(false);
     };
 
     return (
-        <div className="header">
-            <Link to={"/"}>
+        <div className='header'>
+            <Link to={'/'}>
                 {/**
                  *  Image is a img-tag with other properties(zoom in and stuff)
                  * -> decided to use normal img tag */}
                 {/* TODO: use wrapper component to avoid redundant multiplication of components */}
-                <img src={dbLogo} width={150} alt="Website logo" />
+                <img src={dbLogo} width={150} alt='Website logo' />
             </Link>
             {/* h1 headline */}
             <Typography.Title>Test Dashboard</Typography.Title>
             <Space>
-                {/* Notifsication number on top right corner of wrapped icon -> can also just display a "dot" instead of number */}
+                {/* notification number on top right corner of wrapped icon -> can also just display a "dot" instead of number */}
                 <Badge count={10}>
                     <MailOutlined
                         style={{ fontSize: 24 }}
-                        onClick={handleOpenNotifs}
+                        onClick={handleOpenMessages}
                     />
                 </Badge>
                 <Badge count={20}>
                     <BellOutlined
                         style={{ fontSize: 24 }}
-                        onClick={handleOpenMessages}
+                        onClick={handleOpenNotif}
                     />
                 </Badge>
                 <SetMode />
                 {/* drawers */}
                 <Drawer
-                    title="Messages"
-                    open={openNotifs}
-                    onClose={handleCloseNotifs}
-                >
-                    {notifs.map((notif) => (
-                        <Space
-                            key={notif.id}
-                            size={"small"}
-                            direction="vertical"
-                            style={{
-                                display: "flex",
-                                flexDirection: "column",
-                            }}
-                        >
-                            <Card
-                                title={notif.title}
-                                hoverable
-                                style={{ marginBottom: "24px" }}
-                            >
-                                <Space direction="vertical" size={20}>
-                                    <span>{notif.body}</span>
-                                    <Space direction="horizontal">
-                                        {notif.tags.map((tag) => (
-                                            <Tag color="processing" key={tag}>
-                                                {tag}
-                                            </Tag>
-                                        ))}
-                                    </Space>
-                                </Space>
-                            </Card>
-                        </Space>
-                    ))}
-                </Drawer>
-                <Drawer
-                    title="Notifications"
+                    title='Messages'
                     open={openMessages}
                     onClose={handleCloseMessages}
                 >
                     {/* loop over messages and give title and body. Maybe align tags */}
                     {messages.map((message) => (
                         <Space
-                            key={message.id}
-                            size={"small"}
-                            direction="vertical"
+                            size={'small'}
+                            direction='vertical'
                             style={{
-                                display: "flex",
-                                flexDirection: "column",
+                                display: 'flex',
+                                gap: '16px',
+                                flexDirection: 'column',
                             }}
                         >
                             <Card
                                 title={message.title}
                                 hoverable
-                                style={{ marginBottom: "24px" }}
+                                style={{ marginBottom: '24px' }}
                             >
                                 <span>{message.body}</span>
                             </Card>
                         </Space>
                     ))}
+                </Drawer>
+                <Drawer
+                    title='Notifications'
+                    open={openNotif}
+                    onClose={handleCloseNotif}
+                >
+                    jkl
                 </Drawer>
             </Space>
         </div>
